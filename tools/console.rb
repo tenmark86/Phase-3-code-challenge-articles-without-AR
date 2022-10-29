64a55f3e5f3df13c395406fd97149deb9d729778
tenmark86e <tenmark86@gmail.com>
2022-10-29 16:50:47 +0300

    uptaded test cases

--- tools/console.rb
+++ tools/console.rb
@@ -8,8 +8,8 @@ end
 ###  WRITE YOUR TEST CODE HERE ###
 
 # Author
-john = Author.new( "John")
-jane = Author.new( "Jane")
+mark = Author.new( "Mark")
+ann = Author.new( "Ann")
 
 # Magazine
 gq = Magazine.new( "GQ",  "Sports")
@@ -20,16 +20,16 @@ newyork = Magazine.new( "New York Times",  "News")
 newyorker = Magazine.new( "The New Yorker",  "Comic")
 
 # Article
-article1 = Article.new(john,gq, "What Went Down At NFL Last Weekend")
-article2 = Article.new(jane,glamour, "Do We Have a Celebrity & Skin-Care Problem ?")
-article3 = Article.new(john,wired, "Meet The New UK Prime Minister")
-article4 = Article.new(jane,gq, "The Crazy Logistics Of F1")
-article5=Article.new(john,gq, "The Fall Of Manchester United")
-article6=Article.new(jane,newyork, "Politics In Soho")
-article7=Article.new(jane,newyorker, "The Marvel DC Crew")
-article8=Article.new(john,wired, "Queen Elizabeth Is Dead")
-article9=Article.new(john,glamour, "Is SkinCare Gendered?")
-article10=Article.new(jane,glamour, "Newest Foundation techniques")
+article1 = Article.new(mark,gq, "Will Ronaldo leave united on the winter transfer")
+article2 = Article.new(ann,glamour, "what will we expect on the red carpet with the launch of black adam movie ?")
+article3 = Article.new(mark,wired, "Elon musk completes Twitter purchase")
+article4 = Article.new(ann,gq, "Premier league:Haaland the best thing to ever happen to city")
+article5=Article.new(mark,gq, "legue one: mbappe confirms longterm commitment to psg")
+article6=Article.new(ann,newyork, "war in ukraine and its effects in Europe")
+article7=Article.new(ann,newyorker, "DCUE rebrands to DCU as James gunn appointed CEO")
+article8=Article.new(mark,wired, "Queen Elizabeth Is Dead")
+article9=Article.new(mark,glamour, "kanye west looses 2bn dollars in a day")
+article10=Article.new(ann,glamour, "Rihanna releases new music on spotify")
 
 
 
